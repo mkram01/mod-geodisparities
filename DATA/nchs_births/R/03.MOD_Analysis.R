d3c5c969f98c1d501b6b444291a1241aa2a98507
Kevin Weiss <kevin.weiss@emory.edu>
2019-01-30 16:40:06 -0500

    Update R file read-in

--- DATA/nchs_births/R/03.MOD_Analysis.R
+++ DATA/nchs_births/R/03.MOD_Analysis.R
@@ -1,4 +1,4 @@
-# March of Dimes Analysis # 
+# March of Dimes Analysis #
 # 03: Analysis Script     #
 # Kevin Weiss             #
 # 01/07/2019              #
@@ -13,6 +13,13 @@ library(plyr)
 library(tidyr)
 library(magrittr)
 library(cdlTools)
+
+model1 <- readRDS(file = "DATA/nchs_births/R/model1.rda")
+model2 <- readRDS(file = "DATA/nchs_births/R/model2.rda")
+model3 <- readRDS(file = "DATA/nchs_births/R/model3.rda")
+model4 <- readRDS(file = "DATA/nchs_births/R/model4.rda")
+model5 <- readRDS(file = "DATA/nchs_births/R/model5.rda")
+model6 <- readRDS(file = "DATA/nchs_births/R/model6.rda")
 
 # Read in Config file
 config <- load_config(repo = repo,
