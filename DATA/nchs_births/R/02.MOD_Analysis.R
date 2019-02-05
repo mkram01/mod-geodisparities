e0ea6e8c93df75b4b3468a44512e60d1aab42dd8
Kevin Weiss <kevin.weiss@emory.edu>
2019-02-05 10:30:53 -0500

    Add INLA tutorial/test code

--- DATA/nchs_births/R/02.MOD_Analysis.R
+++ DATA/nchs_births/R/02.MOD_Analysis.R
@@ -39,9 +39,57 @@ gatestmodel1 <- readRDS(file = "DATA/nchs_births/R/data/gatestmodel1.rda")
 # Model 6: YEAR x COUNTY x RACE x AGE x MARITAL x EDUCATION (including multiple births)
 
 
+# Zero-inflated Poisson?
+model <- inla(ptb ~ dob_yy + combfips + racehisp_recode, family = c("poisson"), 
+     data = gatestmodel1) 
 
 
-# Read in Config file
+# Testing -----------------------
+formula <- Petal.Length ~ 1 + Petal.Width
+output <- inla(formula, family = "gaussian", data = iris)
+summary(output)
+
+# output commands
+output$summary.fixed
+output$summary.hyperpar
+
+# posterior marginal distribution
+output$marginals.fixed
+output$marginals.hyperpar
+
+# plot posterior distribution of fixed parameter
+beta1_post <- output$marginals.fixed$Petal.Width
+plot(beta1_post, type = "l",xlab = expression(beta[1]),
+       ylab = expression(tilde(p)(paste(beta[1],"|",y))))
+
+# quantile (95% credibility interval)
+inla.qmarginal(0.025, beta1_post)
+inla.qmarginal(0.975, beta1_post)
+
+
+# Hyper parameter
+names(output$marginals.hyperpar)
+prec_post <- output$marginals.hyperpar$"Precision for the Gaussian observations"
+# an equivalent command is
+prec_post <- output$marginals.hyperpar[[1]]
+inla.emarginal(fun = function(x) 1/x, marg = prec_post)
+
+# SD of hyperparameter variance
+m1 <- inla.emarginal(function(x) 1/x, prec_post)
+m2 <- inla.emarginal(function(x) (1/x)^2, prec_post)
+sd <- sqrt(m2 - m1^2)
+sd
+
+# Plot posterior distribution of hyperparameter variance
+# inla.tmarginal transforms given marginal distribution
+plot.default(inla.tmarginal(function(x) 1/x,prec_post),
+             type = "l",xlab = expression(sigmâ2),
+             ylab = xpression(tilde(p)(paste(sigmâ2,"|",y))))
+
+
+
+
+# Read in Config file ------------------
 config <- load_config(repo = repo,
                       indicator_group = indicator_group,
                       indicator = indicator)
