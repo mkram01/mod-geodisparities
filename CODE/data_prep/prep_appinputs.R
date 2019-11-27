f1113542700ffaed6c1e12a773f8714e53c81ed8
erstearns <estearns88@gmail.com>
2019-11-27 10:03:01 -0800

    updated app data prep script to prep mapyears data used for contextual data source year attribution in map captioning in app

--- CODE/data_prep/prep_appinputs.R
+++ CODE/data_prep/prep_appinputs.R
@@ -10,7 +10,7 @@ rm(list = ls())
 # -------------------------------------- set up environment ---------------------------------------- #
 ######################################################################################################
 #load packages
-pacman::p_load(data.table,sf,dplyr,rgeos, tmap, tidyverse)
+pacman::p_load(data.table,sf,dplyr,rgeos, tmap, tidyverse, geojsonsf, geojsonio, tidyr)
 
 # set code repo
 repo <- Sys.getenv('mod_repo')
@@ -24,14 +24,18 @@ message(paste0("You have specified ", data_repo, " as the location of your data.
 #TO-DO -- define app input data for preparation to be pulled into app
 #contextual data & associated data dictionary
 contextual_data <- paste0(data_repo,"/app_inputs/pre_processed_inputs/contextual-data-4november2019.rds")
-contextual_dd <- paste0(data_repo,"/app_inputs/pre_processed_inputs/contextual-metadata.rds")
+#contextual_dd <- paste0(data_repo,"/app_inputs/pre_processed_inputs/contextual-metadata.rds")
+contextual_dd <- paste0(data_repo,"/app_inputs/pre_processed_inputs/contextual-metadata-17November.rds")
 
 #perinatal model data & associated data dictionary
 model_data <- paste0(data_repo,"/app_inputs/pre_processed_inputs/perinatal-data-2november2019.rds")
 model_dd <- paste0(data_repo,"/app_inputs/pre_processed_inputs/perinatal-metadata.rds")
 
+#matrix of variables x years with data source year as cell value -- to be used to dynamically plug year of data source into captioning
+mapyears_data <- paste0(data_repo, "/app_inputs/pre_processed_inputs/map-years.rds")
+
 #base spatial layer
-#base <- paste0(data_repo,"/app_inputs/pre_processed_inputs/us_counties_2017.gpkg")
+base <- paste0(data_repo,"/app_inputs/pre_processed_inputs/us_counties_2017.gpkg")
 
 ######################################################################################################
 # -------------------------------------- load data ------------------------------------------------- #
@@ -41,17 +45,25 @@ acs <- readRDS(contextual_data)
 #load meta data 
 cdd <- readRDS(contextual_dd)
 
+#newcdd <- readRDS(newestcontext)
+
+#for display
+#write.csv(newcdd, file = paste0(data_repo,"/app_inputs/pre_processed_inputs/contextual-metadata-17November.csv"))
+
 #load perinatal outcome model data
 mod <- readRDS(model_data)
 #load meta data
 mdd <- readRDS(model_dd)
+
+#load matrix ov vars with data source year
+mapyears <- as.data.table(readRDS(mapyears_data))
 
 #load state fips codes to get state name in model data
 states <- fread(paste0(data_repo,"/app_inputs/pre_processed_inputs/state_fips_codes.csv"), stringsAsFactors = F,
                 colClasses = list(character=c("state_name", "fips_code")))
 
 #load 2017 US counties spatial layer
-#basegeo <- st_read(base)
+basegeo <- st_read(base)
 
 ######################################################################################################
 # -------------------------------------- basic data cleanup ---------------------------------------- #
@@ -68,68 +80,13 @@ mdd$display_name <- trimws(mdd$display_name)
 # -------------------------------------- create grouping var for dropdown menu -----------------------
 cdd_dt <- as.data.table(cdd)
 mdd_dt <- as.data.table(mdd)
+
 #create plot_type & reverse_palette field in model metadata to match dimensions to context metadata
 mdd_dt <- mdd_dt[, reverse_palette:=FALSE]
-mdd_dt <- mdd_dt[,plot_type:="sequential"]
-
-# ---- contextual var groupings ----
-c_access <- c("Maternity care access (2016)", "Physicians per capita",
-              "OBYNS per capita", "Mental health providers per capita (2007-2015 only)",
-              "Percent women uninsured")
-c_health <- c("Teen birth rate", "% Obese (2010-2015 only)",
-              "% Fair/Poor health (2010-2015 only)",
-              "Opioid prescription rate (2007-2015 only)", 
-              "Drug poisoning mortality rate (2007-2015 only)" 
-              )
-c_hh <- c("% Single parent households", "% Female headed household",
-          "% Housing overcrowding"
-          )
-c_edu <- c("% No high school", 
-           "% No high school, Black", "% No high school, Hispanic", "% No high school, White",
-           "% College, Black", "% College, Hispanic", "% College, White"
-           )
-c_poverty <- c("% Poverty", 
-               "% Poverty, Black", "% Poverty, Hispanic", "% Poverty, White",
-               "Black-White disparity in poverty rate"
-               )
-c_income <- c("Median household income, Black", "Median household income, Hispanic",
-              "Median household income, non-Hispanic White",
-              "White-Black disparity in median HH income",
-              "Income inequality",
-              "Income inequality, Black", "Income inequality, White")
-c_socsupport <- c("% Receiving public assistance", "Neighbor Deprivation Index",
-                  "Index of concentration at extremes (ICE)", 
-                  "Social associations per capita (2007-2015 only)",    
-                  "Social Capital Index",
-                  "Rural-Urban")
-c_soccapital <- c("% Recently moved", "% Owner occupied housing",
-                  "% Black owner occupied housing", "% Hispanic owner occupied housing", 
-                  "% White owner occupied housing", 
-                  "% Housing distress (2014-2015 only)",
-                  "Violent crime rate (2007-2014 only)",
-                  "Racial diversity (entropy; 2010)",
-                  "% Black", "% Hispanic" 
-                  )
-
-#check that all vars assigned a group
-c_veclength <-sum(length(c_access), length(c_health), length(c_hh), length(c_edu), 
-                  length(c_poverty), length(c_income), length(c_socsupport), length(c_soccapital)
-                  )
-if(length(unique(cdd_dt$display_name)) == c_veclength){
-  message("woohoo! all vars have a group assigned")
-} else {
-  message("erm...you didn't assign a group to all your vars...try again.")
-}
-
-#create & assign group var
-cdd_dt <- cdd_dt[display_name %in% c_access, group:="Access to Care"]
-cdd_dt <- cdd_dt[display_name %in% c_health, group:="Health"]
-cdd_dt <- cdd_dt[display_name %in% c_hh, group:="Household"]
-cdd_dt <- cdd_dt[display_name %in% c_edu, group:="Education"]
-cdd_dt <- cdd_dt[display_name %in% c_poverty, group:="Poverty"]
-cdd_dt <- cdd_dt[display_name %in% c_income, group:="Income"]
-cdd_dt <- cdd_dt[display_name %in% c_socsupport, group:="Social support"]
-cdd_dt <- cdd_dt[display_name %in% c_soccapital, group:="Social Capital"]
+mdd_dt <- mdd_dt[palette == "BuPu",plot_type:="sequential"]
+mdd_dt <- mdd_dt[palette == "PRGn",plot_type:="divergent"]
+#change this once hear back from Michael about meaning
+#cdd_dt <- cdd_dt[varname == "MCD", palette := "#CDBCEC + #FFAD81 + #FF772E + #E8001F"]
 
 # ---- model var groupings ----
 m_ptb <- c("Preterm birth, Total",
@@ -162,11 +119,35 @@ if(length(unique(mdd_dt$display_name)) == m_veclength){
 }
 
 #create & assign group var
-mdd_dt <- mdd_dt[display_name %in% m_ptb, group:="Preterm birth"]
-mdd_dt <- mdd_dt[display_name %in% m_vptb, group:="Very preterm birth"]
-mdd_dt <- mdd_dt[display_name %in% m_eptb, group:="Early preterm birth"]
-mdd_dt <- mdd_dt[display_name %in% m_lptb, group:="Late preterm birth"]
-mdd_dt <- mdd_dt[display_name %in% m_etb, group:="Early term birth"]
+mdd_dt <- mdd_dt[display_name %in% m_ptb, category_name:="Preterm birth"]
+mdd_dt <- mdd_dt[display_name %in% m_vptb, category_name:="Very preterm birth"]
+mdd_dt <- mdd_dt[display_name %in% m_eptb, category_name:="Early preterm birth"]
+mdd_dt <- mdd_dt[display_name %in% m_lptb, category_name:="Late preterm birth"]
+mdd_dt <- mdd_dt[display_name %in% m_etb, category_name:="Early term birth"] 
+
+# ---- wrangling map-years ----
+#fix columns named 2015.1 and 2015.2 to read as 2016 and 2017, respectively
+setnames(mapyears, c("X2015.1", "X2015.2"), c("X2016","X2017"))
+mapyears <- mapyears[,"X2016":="X2015.1"]
+mapyears <- mapyears[,"X2017":="X2015.2"]
+
+#remove X from col names
+mapyrs_newnames <- as.character(seq(2007,2017,1))
+mapyrs_oldnames <- names(mapyears)[!names(mapyears) %in% c("X", "varname")]
+setnames(mapyears, mapyrs_oldnames, mapyrs_newnames)
+
+#join to contextual metadata to get display name -- this is what will be used for matching in the app
+mapyears2 <- merge(mapyears, cdd, by = "varname")
+
+# -- reshape long -- 
+#subset to only needed columns
+mapyears3 <- mapyears2[,c("display_name", mapyrs_newnames), with=FALSE]
+
+#reshape long
+mapyears4 <- gather(mapyears3, key = "viz_year", value = "source_year", -display_name)
+
+#convert viz year to integer
+mapyears4$viz_year <- as.integer(mapyears4$viz_year)
 
 ######################################################################################################
 # -------------------------------------- Rename data fields to their human-readable versions ------- #
@@ -229,8 +210,19 @@ acs5 <- acs4[,c(1:2, 53, 54, 4:52)] #GEOID, year, county_name, state_name, ..., 
 mod6 <- mod5[,c(1, 3, 45, 47, 4:44)] #GEOID, year, county_name, state_name, ..., geom
 
 #join together
+# -- convert mod data to dataframe
 mod6dt <- as.data.frame(mod6)
 mod6dt$geom <- NULL
+
+# -- convert acs data to dataframe
+acs5dt <- as.data.frame(acs5)
+acs5dt$geom <- NULL
+
+# -- create simple sf object to join to -- check against original base layer
+
+
+# -- join
+
 allin1 <- left_join(acs5, mod6dt)
 
 ######################################################################################################
@@ -284,22 +276,121 @@ all_sf <- st_cast(all_fin, "MULTIPOLYGON")
 #class(st_geometry(context_sf))
 #class(st_geometry(mod_sf))
 class(st_geometry(all_sf))
+
+#convert to geojson
+#alljson <- sf_geojson(all_sf, options(encoding = "utf8"))
+
 ######################################################################################################
 # -------------------------------------- save spatial data objects ------------------------------- #
 ######################################################################################################
 #save meta data
-saveRDS(cdd_dt, file = paste0(data_repo,"/app_inputs/contextual-metadata-9november2019.rds"))
-saveRDS(mdd_dt, file = paste0(data_repo,"/app_inputs/perinatal-metadata-9november2019.rds"))
+saveRDS(cdd_dt, file = paste0(data_repo,"/app_inputs/contextual-metadata-25november2019.rds"))
+saveRDS(mdd_dt, file = paste0(data_repo,"/app_inputs/perinatal-metadata-13november2019.rds"))
+
+#save map-years data
+saveRDS(mapyears4, file = paste0(data_repo,"/app_inputs/mapyears-27november2019.rds"))
 
 #save app inputs
 #saveRDS(context_sf, file = paste0(data_repo,"/app_inputs/contextual-data-5november2019.rds"))
 #saveRDS(mod_sf, file = paste0(data_repo,"/app_inputs/perinatal-data-5november2019.rds"))
-saveRDS(all_sf, file = paste0(data_repo,"/app_inputs/all-data-9november2019.rds"))
+saveRDS(all_sf, file = paste0(data_repo,"/app_inputs/all-data-13november2019.rds"))
+
+#save json
+#geojson_write(alljson, file = paste0(data_repo,"/app_inputs/all-data-9november2019.geojson"))
 
 
+######################################################################################################
+# ---------------------------------------------- test w/some mapping ------------------------------- #
+######################################################################################################
+basegeo2 <- st_transform(basegeo, crs = 4326)
+
+plot_mapbox(basegeo2, split=~NAME) %>%
+  layout(
+    mapbox = list(style = "open-street-map")
+  )
+
+leaflet(basegeo2) %>%
+  addPolygons()
+
+leaflet(acs5) %>%
+  addPolygons()
+
+# able to successfully plot with leaflet
+acs6 <- st_transform(acs5, crs = 4326)
+
+pal <- colorBin("YlOrRd", domain = acs6$`% Poverty`, bins = 5)
+leaflet(acs6) %>%
+  addTiles() %>%
+  addPolygons(
+    fillColor = ~pal(`% Poverty`),
+    weight = 2, 
+    color = "white",
+    dashArray = "3",
+    fillOpacity = 0.7
+  )
 ######################################################################################################
 # ---------------------------------------------- SCRAP --------------------------------------------- #
 ######################################################################################################
+# ---- contextual var groupings ----
+# c_access <- c("Maternity care access (2016)", "Physicians per capita",
+#               "OBYNS per capita", "Mental health providers per capita (2007-2015 only)",
+#               "Percent women uninsured")
+# c_health <- c("Teen birth rate", "% Obese (2010-2015 only)",
+#               "% Fair/Poor health (2010-2015 only)",
+#               "Opioid prescription rate (2007-2015 only)", 
+#               "Drug poisoning mortality rate (2007-2015 only)" 
+#               )
+# c_hh <- c("% Single parent households", "% Female headed household",
+#           "% Housing overcrowding"
+#           )
+# c_edu <- c("% No high school", 
+#            "% No high school, Black", "% No high school, Hispanic", "% No high school, White",
+#            "% College, Black", "% College, Hispanic", "% College, White"
+#            )
+# c_poverty <- c("% Poverty", 
+#                "% Poverty, Black", "% Poverty, Hispanic", "% Poverty, White",
+#                "Black-White disparity in poverty rate"
+#                )
+# c_income <- c("Median household income, Black", "Median household income, Hispanic",
+#               "Median household income, non-Hispanic White",
+#               "White-Black disparity in median HH income",
+#               "Income inequality",
+#               "Income inequality, Black", "Income inequality, White")
+# c_socsupport <- c("% Receiving public assistance", "Neighbor Deprivation Index",
+#                   "Index of concentration at extremes (ICE)", 
+#                   "Social associations per capita (2007-2015 only)",    
+#                   "Social Capital Index",
+#                   "Rural-Urban")
+# c_soccapital <- c("% Recently moved", "% Owner occupied housing",
+#                   "% Black owner occupied housing", "% Hispanic owner occupied housing", 
+#                   "% White owner occupied housing", 
+#                   "% Housing distress (2014-2015 only)",
+#                   "Violent crime rate (2007-2014 only)",
+#                   "Racial diversity (entropy; 2010)",
+#                   "% Black", "% Hispanic" 
+#                   )
+# 
+# #check that all vars assigned a group
+# c_veclength <-sum(length(c_access), length(c_health), length(c_hh), length(c_edu), 
+#                   length(c_poverty), length(c_income), length(c_socsupport), length(c_soccapital)
+#                   )
+# if(length(unique(cdd_dt$display_name)) == c_veclength){
+#   message("woohoo! all vars have a group assigned")
+# } else {
+#   message("erm...you didn't assign a group to all your vars...try again.")
+# }
+# 
+# #create & assign group var
+# cdd_dt <- cdd_dt[display_name %in% c_access, group:="Access to Care"]
+# cdd_dt <- cdd_dt[display_name %in% c_health, group:="Health"]
+# cdd_dt <- cdd_dt[display_name %in% c_hh, group:="Household"]
+# cdd_dt <- cdd_dt[display_name %in% c_edu, group:="Education"]
+# cdd_dt <- cdd_dt[display_name %in% c_poverty, group:="Poverty"]
+# cdd_dt <- cdd_dt[display_name %in% c_income, group:="Income"]
+# cdd_dt <- cdd_dt[display_name %in% c_socsupport, group:="Social support"]
+# cdd_dt <- cdd_dt[display_name %in% c_soccapital, group:="Social Capital"]
+
+
 # ------ make sure geographies & years overlap -- 11/3 same length ------
 #acsgeos <- unique(acs2$GEOID) 
 #modgeos <- unique(mod2$GEOID)
--- CODE/shiny/run_app.R
+++ CODE/shiny/run_app.R
@@ -3,16 +3,16 @@
 # script objective: profiling shiny app versions
 # date: 23 april 2019
 ###########################################
-
-rm(list = ls())
-
-# Load library
-library(profvis)
-library(shiny)
-library(htmlwidgets)
-
-#for running GeoDisparities locally
-runApp("GeoDisparities")
+  
+  rm(list = ls())
+  
+  # Load library
+  library(profvis)
+  library(shiny)
+  library(htmlwidgets)
+  
+  #for running GeoDisparities locally
+  runApp("GeoDisparities")
 
 
 #for testing and development
